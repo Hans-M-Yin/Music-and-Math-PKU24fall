cc53d3ca5baf23b650e2b8d86f225c0ef763319c
zyy3077 <zhaoyuanyuan@stu.pku.edu.cn>
2024-12-17 19:24:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,15 +3,15 @@
 sudo apt update
 
 # 安装必要的工具（如果尚未安装）
-sudo apt install -y python3-venv python3-pip
+sudo apt install -y python3-venv python3-pip lilypond
 
 # 创建虚拟环境
 python3 -m venv musicMath
 
 # 激活虚拟环境
-source musciMath/bin/activate
+source musicMath/bin/activate
 
 # 安装 Flask 和 music21
-pip install Flask music21 lilypond
+pip install Flask music21 
 
 echo "虚拟环境已创建并安装所需包。"
