c987bd7a5f95c59517b8827ff94b418168fcf5d1
hansm_yin <60255501+Hans-M-Yin@users.noreply.github.com>
2024-12-20 09:53:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,30 @@ def generate_png(style):
 
     print("writing lilypond files...")
     for index, ss in enumerate(s):
+        lilypond_file_path = f'output_{index + 1}.ly'
+        ss.write('lilypond', fp=lilypond_file_path)
+    for index, ss in enumerate(s):
+        lilypond_file_path = f'output_{index + 1}.ly'
+        # 使用 LilyPond 生成乐谱图片
+        print("running lilypond...")
+        process = subprocess.run(
+            [r'lilypond', '--png', lilypond_file_path],
+            capture_output=True, text=True, encoding='utf-8'  # 添加 encoding 参数
+        )
+
+        if process.returncode != 0:
+            print(f"LilyPond 命令执行失败，文件: {lilypond_file_path}")
+
         midi_file_path = 'output.mid'
         mf = midi.translate.music21ObjectToMidiFile(ss)
         mf.open(midi_file_path, 'wb')
         mf.write()
         mf.close()
 
-
         # 创建 FluidSynth 实例，指定 SoundFont 文件
         fluidsynth_path = 'FluidSynth/bin/fluidsynth.exe'  # 替换为你的 FluidSynth 路径
         soundfont_path = 'FluidSynth/soundfont.sf2'  # 替换为你的 SoundFont 路径
-        print(f"FluidSynth 路径: {fluidsynth_path}")
+        print(f"################FluidSynth 路径: {fluidsynth_path}")
         # 创建 FluidSynth 实例，指定 FluidSynth 路径和 SoundFont 文件
         fs = FluidSynth(soundfont_path)
 
@@ -48,26 +61,7 @@ def generate_png(style):
     directory_path = os.path.dirname(os.path.abspath(__file__))
     process_directory(directory_path)
 
-    for index, ss in enumerate(s):
-        lilypond_file_path = f'output_{index + 1}.ly'
-        # 使用 LilyPond 生成乐谱图片
-        print("running lilypond...")
-        # 如果没有添加环境变量需要修改路径
 
-        # process = subprocess.run(
-        #             [r'/usr/bin/lilypond', '--png',  lilypond_file_path],
-        #             capture_output=True, text=True, encoding='utf-8'  # 添加 encoding 参数
-        #         )
-
-        process = subprocess.run(
-                    [r'lilypond', '--png',  lilypond_file_path],
-                    capture_output=True, text=True, encoding='utf-8'  # 添加 encoding 参数
-                    )
-
-        if process.returncode != 0:
-            print(f"LilyPond 命令执行失败，文件: {lilypond_file_path}")
 
     print("所有乐谱图片已生成。")
 
-# if __name__ == "__main__":
-#     generate_png()
